8602c7054db8dce65836600cac725050c69e88d8
TheRealAEgir <laloye@laposte.net>
2026-08-16 13:13:47 +0200

    Quartz sync: Aug 16, 2026, 1:13 PM

--- quartz/plugins/global-navigation/dist/components/index.js
+++
@@ -1,7 +0,0 @@
-import {
-  GlobalNavigation
-} from "../chunk-GY457AGV.js";
-export {
-  GlobalNavigation
-};
-//# sourceMappingURL=index.js.map
--- quartz/plugins/global-navigation/dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-export { GlobalNavigation } from './components/index.js';
-import 'preact';
-import 'preact/jsx-runtime';
-import 'hast';
-import 'vfile';
-import '@quartz-community/types';
-import 'unified';
-import '@quartz-community/utils';
